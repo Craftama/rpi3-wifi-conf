b4330e3aff623f3c29ede0e0cb5b6979e5caf746
Michael Kutý <6du1ro.n@gmail.com>
2018-01-28 19:01:01 +0100

    Add parsing IP option and PEP8.

--- run.py
+++ run.py
@@ -2,11 +2,9 @@
 
 import os
 from bluetooth import *
-from wifi import Cell, Scheme
+from wifi import Cell
 import subprocess
 import time
-
-
 
 
 wpa_supplicant_conf = "/etc/wpa_supplicant/wpa_supplicant.conf"
@@ -29,32 +27,31 @@ def wifi_connect(ssid, psk):
     cmd = 'mv wifi.conf ' + wpa_supplicant_conf
     cmd_result = ""
     cmd_result = os.system(cmd)
-    print cmd + " - " + str(cmd_result)
-
+    print(cmd + " - " + str(cmd_result))
 
     # restart wifi adapter
     cmd = sudo_mode + 'ifdown wlan0'
     cmd_result = os.system(cmd)
-    print cmd + " - " + str(cmd_result)
+    print(cmd + " - " + str(cmd_result))
 
     time.sleep(2)
 
     cmd = sudo_mode + 'ifup wlan0'
     cmd_result = os.system(cmd)
-    print cmd + " - " + str(cmd_result)
+    print(cmd + " - " + str(cmd_result))
 
     time.sleep(10)
 
     cmd = 'iwconfig wlan0'
     cmd_result = os.system(cmd)
-    print cmd + " - " + str(cmd_result)
+    print(cmd + " - " + str(cmd_result))
 
     cmd = 'ifconfig wlan0'
     cmd_result = os.system(cmd)
-    print cmd + " - " + str(cmd_result)
+    print(cmd + " - " + str(cmd_result))
 
     p = subprocess.Popen(['ifconfig', 'wlan0'], stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE)
+                         stderr=subprocess.PIPE)
 
     out, err = p.communicate()
 
@@ -63,9 +60,10 @@ def wifi_connect(ssid, psk):
     for l in out.split('\n'):
         if l.strip().startswith("inet addr:"):
             ip_address = l.strip().split(' ')[1].split(':')[1]
+        elif l.strip().startswith("inet"):
+            ip_address = l.strip().split(' ')[1]
 
     return ip_address
-
 
 
 def ssid_discovered():
@@ -74,71 +72,66 @@ def ssid_discovered():
     wifi_info = 'Found ssid : \n'
 
     for current in range(len(Cells)):
-        wifi_info +=  Cells[current].ssid + "\n"
+        wifi_info += Cells[current].ssid + "\n"
 
+    wifi_info += "!"
 
-    wifi_info+="!"
-
-    print wifi_info
+    print(wifi_info)
     return wifi_info
 
 
-def handle_client(client_sock) :
+def handle_client(client_sock):
     # get ssid
     client_sock.send(ssid_discovered())
-    print "Waiting for SSID..."
-
+    print("Waiting for SSID...")
 
     ssid = client_sock.recv(1024)
-    if ssid == '' :
+    if ssid == '':
         return
 
-    print "ssid received"
-    print ssid
+    print("ssid received")
+    print(ssid)
 
     # get psk
     client_sock.send("waiting-psk!")
-    print "Waiting for PSK..."
-
+    print("Waiting for PSK...")
 
     psk = client_sock.recv(1024)
-    if psk == '' :
+    if psk == '':
         return
 
-    print "psk received"
+    print("psk received")
 
-    print psk
+    print(psk)
 
     ip_address = wifi_connect(ssid, psk)
 
-    print "ip address: " + ip_address
+    print("ip address: " + ip_address)
 
     client_sock.send("ip-addres:" + ip_address + "!")
 
     return
 
 
-
 try:
     while True:
-        server_sock=BluetoothSocket( RFCOMM )
-        server_sock.bind(("",PORT_ANY))
+        server_sock = BluetoothSocket(RFCOMM)
+        server_sock.bind(("", PORT_ANY))
         server_sock.listen(1)
 
         port = server_sock.getsockname()[1]
 
         uuid = "815425a5-bfac-47bf-9321-c5ff980b5e11"
 
-        advertise_service( server_sock, "RPi Wifi config",
-                           service_id = uuid,
-                           service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                           profiles = [ SERIAL_PORT_PROFILE ])
+        advertise_service(server_sock, "RPi Wifi config",
+                          service_id=uuid,
+                          service_classes=[uuid, SERIAL_PORT_CLASS],
+                          profiles=[SERIAL_PORT_PROFILE])
 
-
-        print "Waiting for connection on RFCOMM channel %d" % port
+        print("Waiting for connection on RFCOMM channel %d" % port)
 
         client_sock, client_info = server_sock.accept()
-        print "Accepted connection from ", client_info
+        print("Accepted connection from ", client_info)
 
         handle_client(client_sock)
 
@@ -146,8 +139,8 @@ try:
         server_sock.close()
 
         # finished config
-        print 'Finished configuration\n'
+        print('Finished configuration\n')
 
 
 except (KeyboardInterrupt, SystemExit):
-    print '\nExiting\n'
+    print('\nExiting\n')
